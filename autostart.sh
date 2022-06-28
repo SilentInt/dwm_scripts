e0035e5adee4ab07b9b2faf8e1f087e18d234e89
SilentInt <silentint@foxmail.com>
2022-06-28 17:48:22 +0800

    Remove unnecessary

--- autostart.sh
+++ autostart.sh
@@ -2,13 +2,13 @@
 
 /bin/bash ~/scripts/dwm-status.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
-#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-picom -b
-/bin/bash ~/scripts/tap-to-click.sh &
-/bin/bash ~/scripts/inverse-scroll.sh &
-/bin/bash ~/scripts/setxmodmap-colemak.sh &
+picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b&
+#picom -b
+#/bin/bash ~/scripts/tap-to-click.sh &
+#/bin/bash ~/scripts/inverse-scroll.sh &
 nm-applet &
 xfce4-power-manager &
+fcitx5 &
 #xfce4-volumed-pulse &
-/bin/bash ~/scripts/run-mailsync.sh &
-~/scripts/autostart_wait.sh &
+#/bin/bash ~/scripts/run-mailsync.sh &
+~/scripts/autostart_wait.sh
